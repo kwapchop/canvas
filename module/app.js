ace8e4e2cc6edd0ddb228ea3f9fd3dac47ef031f
Макс <kwapchop@icloud.com>
2021-03-30 00:05:36 +0300

    Работа над созданием игрока. Дополнение основного файла

--- module/app.js
+++ module/app.js
@@ -1,53 +1,56 @@
 import {Layout} from "./layout";
 import {Loop} from "./Loop";
+import {Rect} from "./Rect";
+import {Player} from "./Player";
 
 class App {
     constructor(container) {
         this.layer = new Layout(container)
 
-        this.rect = {
-            x:0,
-            y:0,
-            w:32,
-            h:32,
-            vx:500,
-            vy:500
-        }
-        this.rect2 = {
-            x:99,
-            y:10,
-            w:30,
-            h:30,
-            vx:600,
-            vy:600
-        }
-        new Loop(this.update.bind(this),this.display.bind(this))
-    }
-    update(correction){
-        if(this.rect.x <= 0 && this.rect.vx < 0 || this.rect.x + this.rect.w > this.layer.w && this.rect.vx > 0){
-            this.rect.vx = -this.rect.vx
-        }
-        if(this.rect.y <= 0 && this.rect.vy < 0 || this.rect.y + this.rect.h > this.layer.h && this.rect.vy > 0){
-            this.rect.vy = -this.rect.vy
-        }
-        this.rect.x += this.rect.vx * correction;
-        this.rect.y += this.rect.vy * correction;
+        this.rect = new Rect({});
+        this.rect2 = new Rect({
+            x: 99,
+            y: 99,
+            w: 30,
+            h: 30,
+            vx: 50,
+            vy: 50
+        })
+        this.i = 0;
+        this.player = new Player();
 
-        if(this.rect2.x <= 0 && this.rect2.vx < 0 || this.rect2.x + this.rect2.w > this.layer.w && this.rect2.vx > 0){
-            this.rect2.vx = -this.rect2.vx
-        }
-        if(this.rect2.y <= 0 && this.rect2.vy < 0 || this.rect2.y + this.rect2.h > this.layer.h && this.rect2.vy > 0){
-            this.rect2.vy = -this.rect2.vy
-        }
-        this.rect2.x += this.rect2.vx * correction;
-        this.rect2.y += this.rect2.vy * correction;
+
+        this.loop = new Loop(this.update.bind(this), this.display.bind(this));
     }
-    display(){
-            this.layer.context.clearRect(0,0,this.layer.w,this.layer.h);
-            this.layer.context.fillStyle = 'orange';
-            this.layer.context.fillRect(this.rect.x,this.rect.y,this.rect.w,this.rect.h);
+
+    update(correction) {
+        this.rect.update(this.layer, correction);
+        this.rect2.update(this.layer, correction);
+        this.player.update(this.layer, correction);
+        if (
+            this.player.rect.y <= this.rect2.rect.y + this.rect2.rect.h && this.player.rect.y >= this.rect2.rect.y && this.player.rect.x <= this.rect2.rect.x + this.rect2.rect.w && this.player.rect.x >= this.rect2.rect.x ||
+            // this.player.rect.y <= this.rect2.rect.y + this.rect2.rect.h && this.player.rect.x + this.player.rect.w >= this.rect2.rect.x ||
+            // this.player.rect.y + this.player.rect.h >= this.rect2.rect.y && this.player.rect.x >= this.rect2.rect.x + this.rect2.rect.w ||
+            this.player.rect.y + this.player.rect.h >= this.rect2.rect.y && this.player.rect.y <= this.rect2.rect.y + this.rect2.rect.h && this.player.rect.x + this.player.rect.w >= this.rect2.rect.x && this.player.rect.x <= this.rect2.rect.x + this.rect2.rect.w
+            // this.rect2.rect.y >= this.player.rect.y && this.rect2.rect.y < this.player.rect.y + this.player.rect.w &&
+            // this.rect2.rect.y + this.rect2.rect.w >= this.player.rect.y && this.rect2.rect.y + this.rect2.rect.w < this.player.rect.y + this.player.rect.w ||
+            // this.rect2.rect.x > this.player.rect.x && this.rect2.rect.x < this.player.rect.x + this.player.rect.h &&
+            // this.rect2.rect.x + this.rect2.rect.h > this.player.rect.x && this.rect2.rect.x + this.rect2.rect.h < this.player.rect.x + this.player.rect.h
+        ) {
+            this.loop.stopGame()
+            console.log('Game over')
+        }
+    }
+
+    display() {
+        this.layer.context.clearRect(0, 0, this.layer.w, this.layer.h);
+        this.layer.context.fillStyle = 'orange';
+        this.layer.context.fillRect(this.rect.rect.x, this.rect.rect.y, this.rect.rect.w, this.rect.rect.h);
         this.layer.context.fillStyle = 'green';
-        this.layer.context.fillRect(this.rect2.x,this.rect2.y,this.rect2.w,this.rect2.h);
+        this.layer.context.fillRect(this.rect2.rect.x, this.rect2.rect.y, this.rect2.rect.w, this.rect2.rect.h);
+        this.layer.context.fillStyle = 'blue';
+        this.layer.context.fillRect(this.player.rect.x, this.player.rect.y, this.player.rect.w, this.player.rect.h);
+
     }
 }
 
